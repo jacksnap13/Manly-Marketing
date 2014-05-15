06ca5d7671bb0e90dfdc8f40e78873a0e1333862
jacksnap13 <paulmonroeclark@gmail.com>
2014-05-15 14:43:22 -0400

    Original footer
    Next commit drastically reduces the footer size

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ $(function(){
 	$("#arrowOne").on("click", function(){
 		var thisSection = $(this).parent(),
 				nextSection = thisSection.next("section");
+		thisSection.animate({left: "-200%"}, 500);
 		nextSection.css("display", "block").animate({left: "0px"}, 500);
 		$("#hunter").animate({right: "5%"}, 900);
 		$("#quizcontainer1").delay(700).animate({top: "-2%"}, 700);
-		$(".answer.one").delay(2500).animate({top: "200px"}, 600);
-		$(".answer.two").delay(2900).animate({top: "275px"}, 600);
-		$(".answer.three").delay(3200).animate({top: "350px"}, 700);
-		$(".answer.four").delay(3500).animate({top: "425px"}, 800);
+		$(".answer.one").delay(1000).animate({top: "260px"}, 600);
+		$(".answer.two").delay(1400).animate({top: "330px"}, 600);
+		$(".answer.three").delay(1700).animate({top: "400px"}, 700);
+		$(".answer.four").delay(1900).animate({top: "470px"}, 800);
 	});
 	
 	$(".quizcontainer p").on("mouseover", function(){
@@ -20,19 +21,25 @@ $(function(){
 		$(this).find("span").css("background-color", "#5c5c5c");
 	});
 	
-	$(".answer").on("click", function(){
+	$("#quizcontainer1 .answer").on("click", function(){
 		var answer = $(this).data("answer");
-		$("#modalOne").fadeIn(500);
+		$(".quizcontainer").animate({top: "-2000"}, 800);
+		$("#hunter").delay(300).animate({top: "5000"}, 1200);
+		$(".hotprod").delay(300).animate({top: "150px"}, 600);
+		$(".heatmaxlogo").delay(400).animate({bottom: "30%"}, 600);
+		$("#firstAnswer").delay(400).animate({top: "0"}, 600);
+		$("#arrowTwo").delay(1200).fadeIn(900);
 		if(answer == "right"){
-			$(".dialogbox p").html("You is so smart");
+			$(".correct.first").html("Correct! Good job.");
 		}
-		else{$(".dialogbox p").html("You is so wrong");}
+		
 	});
 	
 	$("#arrowTwo").on("click", function(){
 		var mother = $(this).parents("section"),
 		nextMom = mother.next("section");
-		nextMom.css("display", "block").animate({left: "0px"}, 500);
+		mother.animate({left: "-200%"},600);
+		nextMom.show().animate({left: "0px"}, 500);
 	});
 	
 });
